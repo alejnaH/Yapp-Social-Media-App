709b0bb9745554a2620a627c07dd390f5ce7ac05
Alejna <alejna.hasanagic@stu.ibu.edu.ba>
2025-05-04 17:30:24 +0200

    deleted unecessary testing file

--- backend/public/v1/docs/swagger-simple.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . '/../../../vendor/autoload.php';
-
-$openapi = \OpenApi\Generator::scan([__DIR__ . '/../../../']);
-
-header('Content-Type: application/json');
-echo $openapi->toJson();
